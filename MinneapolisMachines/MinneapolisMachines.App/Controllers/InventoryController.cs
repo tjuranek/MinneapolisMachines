20f6605cb631caf918da532a20d1f97940fde318
tjuranek <thomas@juranek.com>
2019-10-28 22:10:23 -0500

    inventory displaying proper vehicles and their properties

--- MinneapolisMachines/MinneapolisMachines.App/Controllers/InventoryController.cs
+++ MinneapolisMachines/MinneapolisMachines.App/Controllers/InventoryController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using System.Linq;
 using MinneapolisMachines.App.Models.Admin;
 using MinneapolisMachines.App.Models.Factories;
+using MinneapolisMachines.App.Models.Inventory;
 using MinneapolisMachines.Data.Interfaces;
 using MinneapolisMachines.Models.Vehicles;
 
@@ -13,15 +15,36 @@ namespace MinneapolisMachines.App.Controllers
         public ActionResult New()
         {
             IVehiclesRepo vehiclesRepo = RepoFactory.CreateVehiclesRepo();
-            List<Vehicle> vehicles = vehiclesRepo.GetAll();
 
-            return View(vehicles);
+            return View(new InventoryViewModel()
+            {
+                Vehicles = vehiclesRepo.GetAll().Where(v => v.BodyTypeId == 1).ToList(),
+                Makes = vehiclesRepo.GetMakes(),
+                Models = vehiclesRepo.GetModels(),
+                InteriorColors = vehiclesRepo.GetInteriorColors(),
+                ExteriorColors = vehiclesRepo.GetExteriorColors(),
+                BodyTypes = vehiclesRepo.GetBodyTypes(),
+                BodyStyles = vehiclesRepo.GetBodyStyles(),
+                TransmissionTypes = vehiclesRepo.GetTransmissionTypes()
+            });
         }
 
         // GET: Used
         public ActionResult Used()
         {
-            return View();
+            IVehiclesRepo vehiclesRepo = RepoFactory.CreateVehiclesRepo();
+
+            return View(new InventoryViewModel()
+            {
+                Vehicles = vehiclesRepo.GetAll().Where(v => v.BodyTypeId == 2).ToList(),
+                Makes = vehiclesRepo.GetMakes(),
+                Models = vehiclesRepo.GetModels(),
+                InteriorColors = vehiclesRepo.GetInteriorColors(),
+                ExteriorColors = vehiclesRepo.GetExteriorColors(),
+                BodyTypes = vehiclesRepo.GetBodyTypes(),
+                BodyStyles = vehiclesRepo.GetBodyStyles(),
+                TransmissionTypes = vehiclesRepo.GetTransmissionTypes()
+            });
         }
 
         // GET: Details / {id}
---
+++ MinneapolisMachines/MinneapolisMachines.App/Models/Inventory/InventoryViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MinneapolisMachines.Models.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinneapolisMachines.App.Models.Inventory
+{
+    public class InventoryViewModel { 
+    
+        public List<Vehicle> Vehicles { get; set; }
+
+        public List<Make> Makes { get; set; }
+
+        public List<Model> Models { get; set; }
+
+        public List<VehicleProperty> BodyTypes { get; set; }
+
+        public List<VehicleProperty> BodyStyles { get; set; }
+
+        public List<VehicleProperty> TransmissionTypes { get; set; }
+
+        public List<VehicleProperty> InteriorColors { get; set; }
+
+        public List<VehicleProperty> ExteriorColors { get; set; }
+
+    }
+}
