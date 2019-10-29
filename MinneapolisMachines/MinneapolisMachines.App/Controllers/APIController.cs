3a05907a1061cec52666448d1707fdaa51678fdb
tjuranek <thomas@juranek.com>
2019-10-29 14:38:18 -0500

    search feature

--- MinneapolisMachines/MinneapolisMachines.App/Controllers/APIController.cs
+++ MinneapolisMachines/MinneapolisMachines.App/Controllers/APIController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Http;
 using MinneapolisMachines.Data.Interfaces;
 using MinneapolisMachines.App.Models.Factories;
+using MinneapolisMachines.Models.Vehicles;
 
 namespace MinneapolisMachines.App.Controllers
 {
@@ -43,5 +44,44 @@ namespace MinneapolisMachines.App.Controllers
 
             return Ok(vehicles);
         }
+
+        [Route("api/search/{keyword}/{minPrice}/{maxPrice}/{minYear}/{maxYear}")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult Search(string keyword, decimal minPrice, decimal maxPrice, int minYear, int maxYear)
+        {
+            IVehiclesRepo vehiclesRepo = RepoFactory.CreateVehiclesRepo();
+            SearchType searchType = int.TryParse(keyword, out int year) ? SearchType.Year : SearchType.MakeModel;
+
+            if (searchType == SearchType.Year)
+                return Ok(vehiclesRepo.GetAll().Where(v => v.ReleaseYear == year).Where(v => v.SalesPrice >= minPrice && v.SalesPrice <= maxPrice));
+
+            var vehicles = vehiclesRepo.GetAll();
+            var makes = vehiclesRepo.GetMakes();
+            var models = vehiclesRepo.GetModels();
+            var modelSearch = new List<Vehicle>();
+            var makeSearch = new List<Vehicle>();
+
+            // this gets all of the vehicles where there is a model name containing the keyword, and is in the price range and release year range
+            try
+            {
+                modelSearch = vehicles.Where(v => v.ModelId == models.FirstOrDefault(m => m.Name.ToLower().Contains(keyword.ToLower())).Id).Where(v => v.SalesPrice >= minPrice && v.SalesPrice <= maxPrice).Where(v => v.ReleaseYear >= minYear && v.ReleaseYear <= maxYear).ToList();
+            }
+            catch (Exception e)
+            {
+                modelSearch = new List<Vehicle>();
+            }
+            
+
+            // this gets all of the vehicles where the make id matches a model id, searches the make name for the keyword, and filters by price and year range
+            makeSearch = vehicles.Where(v => makes.FirstOrDefault(ma => ma.Id == models.FirstOrDefault(m => m.Id == ma.Id).Id).Name.ToLower().Contains(keyword.ToLower())).Where(v => v.SalesPrice >= minPrice && v.SalesPrice <= maxPrice).Where(v => v.ReleaseYear >= minYear && v.ReleaseYear <= maxYear).ToList();
+
+            return Ok(modelSearch.Count >= makeSearch.Count ? modelSearch : makeSearch);
+        }
+    }
+
+    public enum SearchType
+    {
+        Year,
+        MakeModel
     }
 }
